496935c08cffd2da980b956911bb2a2e71b0da34
Karan Sah <karan.sah@enquero.com>
2019-06-17 14:31:29 +0530

    updated 6th question as per requirements

--- 6.js
+++ 6.js
@@ -1,7 +1,30 @@
 people = [
-  { firstname: "Mahatma", lastname: "Candhi", age: 78 },
+  { firstname: "Mahatma", lastname: "Candhi", age: 79 },
   { firstname: "Goutam", lastname: "buddha", age: 80 },
-  { firstname: "koutam", lastname: "buddha", age: 90 }
+  { firstname: "aoutam", lastname: "buddha", age: 90 },
+  { firstname: "cutam", lastname: "buddha", age: 93 },
+  { firstname: "outam", lastname: "buddha", age: 44 }
 ];
-group = (arr, method) => {};
-group(people, "age");
+group = (arr, method) => {
+  if (method == "age") {
+    return arr.sort(function(a, b) {
+      //console.log(JSON.stringify(a)," " ,JSON.stringify(b))
+      //  console.log(a.age - b.age);
+      return a.age - b.age;
+    });
+  } else if (method == "firstname") {
+    //  console.log('working')
+    return arr.sort(function(a, b) {
+      var nameA = a.firstname.toLowerCase(),
+        nameB = b.firstname.toLowerCase();
+      if (nameA < nameB)
+        //sort string ascending
+        return -1;
+      if (nameA > nameB) return 1;
+      return 0; //default return value (no sorting)
+    });
+  }
+};
+//group(people,'age')
+console.log(group(people, "firstname"));
+console.log(group(people, "age"));
